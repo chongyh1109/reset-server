12f5ae2cebe8d415599bd973f6cf6bb653ff7ed0
chongyh1109 <chongyh2006@gmail.com>
2026-03-05 01:13:07 +0800

    fix port

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-// 发送验证码
 app.post("/send-code", async (req, res) => {
   const { email } = req.body;
 
@@ -48,6 +47,8 @@ app.post("/verify-code", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
 });
