757e84e8fb5f2dd5a70a044c12b4bb18b9fc91ff
chongyh1109 <chongyh2006@gmail.com>
2026-03-07 18:22:29 +0800

    fix gmail smtp ipv4\

--- server.js
+++ server.js
@@ -15,10 +15,18 @@ app.get("/", (req, res) => {
 });
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  }
+
+  tls: {
+      rejectUnauthorized: false
   }
 });
 
