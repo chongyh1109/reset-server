915df1686f35f9a1ee0e405c8351aa32f49ff61a
chongyh1109 <chongyh2006@gmail.com>
2026-03-07 18:53:51 +0800

    fix smtp syntax

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
 
   tls: {
       rejectUnauthorized: false
