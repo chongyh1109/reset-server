4e1d1d7dcc3c38da71ea9ac11ad9a82f9d0f0269
chongyh1109 <chongyh2006@gmail.com>
2026-03-08 13:34:19 +0800

    force ipv4 smtp

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
+
+  family: 4
 });
 
 app.post("/send-code", async (req, res) => {
